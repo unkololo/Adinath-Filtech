f206869db5b718e351d0d848d7c36afaaadc4789
unkololo <148550447+unkololo@users.noreply.github.com>
2024-02-19 14:12:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function toggle(){
+    let a = document.querySelector(".nav-bar")
+
+a.classList.toggle("visible")
+
+}
+function vanish(){
+    let a = document.querySelector(".nav-bar")
+
+a.classList.remove("visible")
+}
